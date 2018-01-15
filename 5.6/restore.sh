928eff4098c2e1f06ee9f30f4b5f07c8694fc9c5
Peter Golm <golm.peter@gmail.com>
2018-01-15 09:17:00 +0100

    Add customization of restore options

--- 5.6/restore.sh
+++ 5.6/restore.sh
@@ -2,7 +2,7 @@
 if [ -f /backup/before-restore.sh ]; then source /backup/before-restore.sh; fi
 
 echo -n "Restore backup..."
-mysql -h $MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD --database $MYSQL_DB < $BACKUP_FILE
+mysql -h $MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD --database $MYSQL_DB $RESTORE_OPTIONS < $BACKUP_FILE
 echo "done"
 
 if [ -f /backup/after-restore.sh ]; then source /backup/after-restore.sh; fi
